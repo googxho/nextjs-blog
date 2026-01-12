747def342836b69f42d9a34f333e60a48707544d
googxho <gxh522@qq.com>
2026-01-12 18:31:15 +0800

    chapter 11-20 #blog 架构

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,30 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
 import Layout from '../../components/layout';
- 
+import Alert from '../../components/alert';
+
 export default function FirstPost() {
   return (
     <Layout>
       <Head>
-        <title>First Post</title>
+        <title>详情页</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        onError={(error) => {
+          console.log("[nku]log >> :19 >> FirstPost >> error:", error);
+        }}
+      />
+      <Alert type={'error'}>
+        状态错误！
+      </Alert>
+
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">← Back to home</Link>
-      </h2>
     </Layout>
   );
 }
